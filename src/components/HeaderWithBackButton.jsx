caf5d19212868a848f886b2a8902ca99770fac8d
gahlot1999 <gahlot1999@gmail.com>
2024-03-17 22:20:33 +0530

    added img

--- src/components/HeaderWithBackButton.jsx
+++ src/components/HeaderWithBackButton.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom';
+import backBtn from '../assets/arrow-left.png';
 
 function HeaderWithBackButton({ title }) {
   const navigate = useNavigate();
   return (
     <div className='relative'>
       <img
-        src='/arrow-left.png'
+        src={backBtn}
         alt='back button icon'
         className='w-[3rem] absolute top-[50%] left-4 translate-y-[-50%]'
         onClick={() => navigate(-1)}
