775e64c1269053944a7e7e042dfef82f8a73ce83
gahlot1999 <gahlot1999@gmail.com>
2024-03-17 20:44:24 +0530

    updated design

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+function Button({ variant = 'primary', children }) {
+  const baseStyles = 'py-5 rounded-[1.6rem]';
+  let activeStyles = '';
+
+  switch (variant) {
+    case 'primary':
+      activeStyles = 'bg-violet-100 text-light-80';
+      break;
+
+    case 'secondary':
+      activeStyles = 'bg-violet-20 text-violet-100';
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <button className={`${baseStyles} ${activeStyles}`}>{children}</button>
+  );
+}
+
+export default Button;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import landingPageImg1 from '/landingPage1.png';
 import landingPageImg2 from '/landingPage2.png';
 import landingPageImg3 from '/landingPage3.png';
+import Button from '../components/Button';
 
 const carouselData = [
   {
@@ -40,18 +41,16 @@ function Landing() {
   }, []);
 
   return (
-    <div className='h-screen p-10 flex flex-col justify-between'>
-      <div className='flex flex-1 flex-col items-center justify-center gap-4 text-center'>
-        <img src={activeItem.img} alt='carlousel image' className='w-[50%]' />
+    <div className='flex flex-col h-screen p-10'>
+      <div className='flex flex-col items-center justify-center flex-1 gap-4 text-center'>
+        <img src={activeItem.img} alt='carlousel image' className='w-[60%]' />
         <div className='grid grid-rows-[9rem_6rem_4rem] items-center justify-items-center'>
           <p className='font-bold leading-[1.1] text-title-lg text-dark-100'>
             {activeItem.title}
           </p>
-          <div className='max-w-[22rem] m-auto'>
-            <p className='text-dark-25 leading-[1.25] text-small'>
-              {activeItem.subTitle}
-            </p>
-          </div>
+          <p className='text-dark-25 max-w-[22rem] leading-[1.25] text-small'>
+            {activeItem.subTitle}
+          </p>
           <div className='flex items-center gap-12'>
             {carouselData.map((el, i) => (
               <div
@@ -67,16 +66,11 @@ function Landing() {
           </div>
         </div>
       </div>
-      <div className='flex flex-col gap-8 text-small font-semibold'>
-        <button className='py-5 rounded-[1.6rem] bg-violet-100 text-light-80'>
-          New Expense
-        </button>
-        <button className='py-5 rounded-[1.6rem] bg-violet-20 text-violet-100'>
-          Old Expense
-        </button>
+      <div className='flex flex-col gap-8 font-semibold text-small'>
+        <Button>New Expense</Button>
+        <Button variant='secondary'>Old Expense</Button>
       </div>
     </div>
   );
 }
-
 export default Landing;
