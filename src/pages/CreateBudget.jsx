0a76a93b3f82e2f09b403bdc9e83e49172227dd4
gahlot1999 <gahlot1999@gmail.com>
2024-03-18 18:38:11 +0530

    asd

--- src/pages/CreateBudget.jsx
+++ src/pages/CreateBudget.jsx
@@ -13,7 +13,7 @@ function CreateBudget() {
 
   return (
     <div className='bg-blue-100 h-screen text-light-100 flex flex-col'>
-      <div className='h-[30rem] p-10 flex flex-col justify-between'>
+      <div className='h-[30rem] p-10 flex-1 flex flex-col justify-between'>
         <HeaderWithBackButton title='Add Budget' />
         <div>
           <Label variant='big' htmlFor='budgetName'>
@@ -28,14 +28,14 @@ function CreateBudget() {
           />
         </div>
       </div>
-      <div className='bg-light-100 flex-1 overflow-y-auto p-10 rounded-[3.2rem_3.2rem_0_0]'>
+      <div className='bg-light-100 overflow-y-auto p-10 rounded-[3.2rem_3.2rem_0_0]'>
         <form
           onSubmit={handleSubmit(submitForm)}
           className='flex flex-col gap-4'
         >
           <Input placeholder='Amount' type='number' inputMode='numeric' />
           <Input placeholder='Description' />
-          <Button>Add</Button>
+          <Button style={{ marginTop: '1.5rem' }}>Add</Button>
         </form>
       </div>
     </div>
