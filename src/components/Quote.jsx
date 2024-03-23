d3fb91da77cd7c646ab20d36a3c7cf6ababc6ba2
gahlot1999 <gahlot1999@gmail.com>
2024-03-24 02:35:57 +0530

    made login page responsive

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,6 +1,6 @@
 function Quote({ quote }) {
   return (
-    <div className='grid place-items-center relative isolate before:content-[open-quote] before:font-serif before:font-black before:absolute before:top-[-10rem] lg:before:top-[0rem] before:left-[-0.5rem] before:z-[-1] before:text-[25rem] before:text-violet-40'>
+    <div className='grid place-items-center lg:mx-32 relative isolate before:content-[open-quote] before:font-serif before:font-black before:absolute max-lg:before:top-[-10rem] max-lg:before:left-[-0.5rem] before:left-0 before:z-[-1] before:text-[25rem] before:text-violet-40'>
       <p
         className={`leading-snug text-center p-16 font-bold text-dark-50 ${
           quote?.length > 80 ? 'text-title-md' : 'text-title-lg'
